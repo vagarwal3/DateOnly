6cff666a83490d7e80e0e3c794414370c7cd902a
vagarwal3 <121586291+vagarwal3@users.noreply.github.com>
2023-01-16 12:40:31 -0500

    Initial code

--- DateOnly.ts
+++ DateOnly.ts
@@ -1,1 +1,43 @@
+class DateOnly
+{
+    Day:number;
+    Month:number;
+    Year:number;
+    constructor(month:number,day:number,year:number)
+    {
+        this.Month=month;
+        this.Day=day;
+        this.Year=year;
+    }
+    DayOfWeek:number;
+    DayOfYear:number;
+    AddDays(days:number):DateOnly
+    {
 
+    }
+    AddMonths(months:number):DateOnly
+    {
+
+    }
+    AddYears(years:number):DateOnly
+    {
+
+    }
+    Equals(a:DateOnly):boolean
+    {}
+    ToString()
+    {}
+    GreaterThanOrEqual(a:DateOnly):boolean
+    {}
+    GreaterThan(a:DateOnly):boolean
+    {}
+    LessThan(a:DateOnly):boolean
+    {}
+    LessThanOrEqual(a:DateOnly):boolean
+    {}
+    NumerOfDays(a:DateOnly,b:DateOnly,areBothDatesInclusive:boolean):number
+    {
+
+    }
+
+}
